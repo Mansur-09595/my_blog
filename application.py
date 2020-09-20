419173c04dcff0ab4a840b4bdf5fd5d2d8012fa7
Mansur-09595 <mansur_musaev_1997@mail.ru>
2020-09-20 21:04:14 +0300

    first commit

---
+++ application.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+def about():
+    return render_template("about.html")
+
+@app.route("/index", methods=["GET","POST"])
+def index():
+    if session.get('notes') is None:
+        session['notes'] = []
+    if request.method == "POST":
+        note = request.form.get("note")
+        session['notes'].append(note)   
+
+    return render_template("index.html", notes=session['notes'])
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    take = request.form.get("name")
+    text = take
+    with open('text.txt', 'w', encoding="utf-8") as txt_file:
+        txt_file.write(text)
+    return render_template("hello.html", message=take)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
